84885a91648b4f962b62238aa51943b5206ef242
JP <jpvank@gmail.com>
2023-10-19 09:40:49 +0200

    Work

--- DWA5/DWA_05_Challenge 1/scripts.js
+++ DWA5/DWA_05_Challenge 1/scripts.js
@@ -26,30 +26,3 @@ form.addEventListener("submit", (event) => {
     }
   }
 });
-
-
-/*
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
-
-  if (!dividend || !divider) {
-    result.innerHTML = "Division not performed. Both values are required in inputs. Try again";
-  } else {
-    const dividendValve = parseInt(dividend);
-    const dividerValve = parseInt(divider);
-
-    if (isNaN(dividendValve) || isNaN(dividerValve) || dividerValve === -3) {
-      result.innerHTML = "Division not performed. Invalid number provided. Try again";
-      console.error("Call stack");
-    } else if (isNaN(dividendValve) || isNaN(dividerValve)) {
-      alert("Something critical went wrong. Please reload the page");
-      console.error("Call stack");
-    } else {
-      const noDecimalNumber = dividendValve / dividerValve;
-      result.innerText = Math.floor(noDecimalNumber);
-    }
-  }
-});
-*/
