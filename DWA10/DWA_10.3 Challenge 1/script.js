e4b94aa1893a11dfe74742330a482be6470525ec
JP <jpvank@gmail.com>
2023-11-20 14:36:28 +0200

    work

---
+++ DWA10/DWA_10.3 Challenge 1/script.js
@@ -0,0 +1,37 @@
+let count = 0;
+const number = document.querySelector('[data-key="counter"]');
+const increase = document.querySelector('[data-key="Add"]');
+const decrease = document.querySelector('[data-key="subtract"]');
+const reset = document.querySelector('[data-key="reset"]');
+
+const increaseHandler = () => {
+    count = count + 1
+    updateCounter();
+}
+const decreaseHandler = () => {
+    // if (count > 0) {
+    //    count = count - 1 
+    //    updateCounter();
+    count = count - 1
+    updateCounter();
+}
+
+const updateCounter = () => {
+    number.textContent = count;
+}
+
+const resetHandler = () => {
+    if (count !== 0) {
+        const confirmedMessage = confirm('Are you sure you want to reset the counter?')
+        if (confirmedMessage) {
+            count = 0;
+            updateCounter();
+        }
+    } else {
+        alert('Counter is already at 0');
+    }
+}
+
+increase.addEventListener('click', increaseHandler);
+decrease.addEventListener('click', decreaseHandler);
+reset.addEventListener('click', resetHandler);
